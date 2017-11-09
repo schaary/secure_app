53b92b1d397a280d7e52b317afd9fe359732cbc4
Michael Schaarschmidt <michael@schaaryworks.com>
2017-11-09 21:17:30 +0100

    add local certificates and switch to https

--- lib/secure_app.ex
+++ lib/secure_app.ex
@@ -2,9 +2,17 @@ defmodule SecureApp do
   use Application
 
   def start(_type, _args) do
+
+    cowboy_options = [
+      keyfile: "priv/keys/localhost.key",
+      certfile: "priv/keys/localhost.cert",
+      otp_app: :secure_app,
+      port: 4040
+    ]
+
     children = [
       Plug.Adapters.Cowboy.child_spec(
-        :http, SecureApp.HelloPlug, [], port: 8080
+        :https, SecureApp.HelloPlug, [], cowboy_options
       )
     ]
 
